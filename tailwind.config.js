b666dbf051889e502f40dd1cf7c8c160ed65c088
Ruth-Ya <easytex.solutions@gmail.com>
2025-11-12 13:21:05 +0000

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: {
+          50:  "#F2F7FB",
+          400: "#3A97E8",
+          500: "#3384CA",
+          600: "#2B70AB",
+        },
+      },
+    },
   },
   plugins: [],
-}
+};
+
